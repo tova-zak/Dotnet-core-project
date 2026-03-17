7142c3e4e400919d3599244b6ab1b68a6f7c21c6
LEAH91R <leahr9386@gmail.com>
2026-03-17 22:48:24 +0200

    design

--- IceCream/obj/Debug/net9.0/IceCream.AssemblyInfo.cs
+++ IceCream/obj/Debug/net9.0/IceCream.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IceCream")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17958a891f216e7bdbb345311fe04197a535395")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c84b874c724a4014368c4a95efe58db21fe8b705")]
 [assembly: System.Reflection.AssemblyProductAttribute("IceCream")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IceCream")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
